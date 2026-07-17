add2d50ee98f8710c523f357a1a8bfb6fb05fc1c
Dev-subha-stack <romendraroy4@gmail.com>
2026-07-17 20:49:59 +0530

    Release updated app version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'nexus-v1.0.0';
+const CACHE_NAME = 'MyApp-v1.0.0';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './offline.html'];
 
 self.addEventListener('install', event => {
